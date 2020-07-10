2b6950b03085a737e1e74ec0c9f3d024d3827ff0
vc64 <vchen2022@spyponders.com>
2020-07-10 13:17:47 -0400

    updated static

---
+++ static/scripts/text.js
@@ -0,0 +1,8 @@
+function myFunction() {
+  var x = document.createElement("image");
+  x.setAttribute("src", "static/images/ace_of_spades.png");
+  x.setAttribute("width", "304");
+  x.setAttribute("height", "228");
+  x.setAttribute("alt", "The Pulpit Rock");
+  document.body.appendChild(x);
+}
